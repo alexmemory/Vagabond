59f4da86f9fab40ce910a35162fc0b9a1ab37591
glavic <glavic@de4125fd-08af-4ef3-add0-b7c1c68fae66>
2011-03-17 21:47:51 +0000

    git-svn-id: https://dblab.cs.toronto.edu/svn/boriscode/TrampExGen/trunk@34 de4125fd-08af-4ef3-add0-b7c1c68fae66

---
+++ test/org/vagabond/test/explanations/TestQueryHolder.java
@@ -0,0 +1,26 @@
+package org.vagabond.test.explanations;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.vagabond.explanation.generation.QueryHolder;
+import org.vagabond.test.AbstractVagabondTest;
+
+public class TestQueryHolder extends AbstractVagabondTest {
+
+	static Logger log = Logger.getLogger(TestQueryHolder.class);
+	
+	@Test
+	public void testLoad () throws FileNotFoundException, IOException {
+		QueryHolder.getInstance().loadFromDir(new File("resource/queries/"));
+		
+		log.debug(QueryHolder.getInstance().getQueries().stringPropertyNames());
+		assertTrue(QueryHolder.hasQuery("CopyCS.GetProv"));
+	}
+	
+}
