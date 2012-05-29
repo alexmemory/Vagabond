305bf9c1487dacbe310e38c1fa9baf0bc67058f7
chientzu <chientzu@de4125fd-08af-4ef3-add0-b7c1c68fae66>
2012-05-29 21:20:31 +0000

    Changed TestBitMarkerPerformance
    git-svn-id: https://dblab.cs.toronto.edu/svn/boriscode/TrampExGen/trunk@216 de4125fd-08af-4ef3-add0-b7c1c68fae66

--- performanceTest/org/vagabond/performance/bitmarker/TestBitMarkerPerformance.java
+++ performanceTest/org/vagabond/performance/bitmarker/TestBitMarkerPerformance.java
@@ -30,11 +30,7 @@ public class TestBitMarkerPerformance {
 		//loadToDB("resource/exampleScenarios/homeless.xml");
 		loadToDB("resource/exampleScenarios/performanceTest1.xml");
 		IMarkerSet bitset1 = MarkerFactory.newBitMarkerSet();
-		IMarkerSet bitset2 = MarkerFactory.newBitMarkerSet();
 		IMarkerSet markerset1 = MarkerFactory.newMarkerSet();
-		IMarkerSet markerset2 = MarkerFactory.newMarkerSet();
-
-		
 		
 		System.out.println("------BitSet Adding Test------");
 		AddingTest(bitset1);
@@ -46,146 +42,24 @@ public class TestBitMarkerPerformance {
 		System.out.println("------MarkerSet Containing Test------");
 		ContainingTest(markerset1);
 		
-		
-//		resetBitSet(bitset1, bitset2);
-//		resetMarkerSet(markerset1, markerset2);
+		System.out.println("------Union Test------");
 		UnionTest();
-//		System.out.println("------MarkerSet Union Test------");
-//		UnionTest(markerset1, markerset2);
+		System.out.println("------Intersect Test------");
+		IntersectTest();
+		System.out.println("------Difference Test------");
+		DiffTest();
+		System.out.println("------Clone Test------");
+		CloneTest();
 		
-		resetBitSet(bitset1, bitset2);
-		resetMarkerSet(markerset1, markerset2);
-		System.out.println("------BitSet Intesect Test------");
-		IntersectTest(bitset1, bitset2);
-		System.out.println("------MarkerSet Intersect Test------");
-		IntersectTest(markerset1, markerset2);
-		
-		resetBitSet(bitset1, bitset2);
-		resetMarkerSet(markerset1, markerset2);
-		System.out.println("------BitSet Diff Test------");
-		DiffTest(bitset1, bitset2);
-		System.out.println("------MarkerSet Diff Test------");
-		DiffTest(markerset1, markerset2);
-		
-		resetBitSet(bitset1, bitset2);
-		resetMarkerSet(markerset1, markerset2);
-		System.out.println("------BitSet Clone Test------");
-		CloneTest(bitset1, bitset2);
-		System.out.println("------MarkerSet Clone Test------");
-		CloneTest(markerset1, markerset2);
-		
-		
-		
-		
-		
-		
-	}
-	public static void  CloneTest(IMarkerSet set1, IMarkerSet set2) throws Exception{
-		Random number = new Random();
-		int maxRel = 3, maxAttr = 3, maxTid = 99999;
-		
-		setToElement(set2, 1000, maxRel, maxTid, maxAttr, number);
-		
-		long test1start = System.currentTimeMillis();
-		for(int i = 0; i< 10; i++)
-			set1 = set2.cloneSet();
-		long test1end = System.currentTimeMillis();
-		
-		
-		long test2start = System.currentTimeMillis();
-		for(int i = 0; i< 100; i++)
-			set1 = set2.cloneSet();
-		long test2end = System.currentTimeMillis();
-		
-		
-		long test3start = System.currentTimeMillis();
-		for(int i = 0; i< 1000; i++)
-			set1 = set2.cloneSet();
-		long test3end = System.currentTimeMillis();
-		
-
-		log.debug("Clone a Sets of 1000 elements 10 time: " + (test1end - test1start));
-		log.debug("Clone a Sets of 1000 elements 100 time: " + (test2end - test2start));
-		log.debug("Clone a Sets of 1000 elements 1000 time: " + (test3end - test3start));
 		
 	}
 	
-	public static void resetBitSet(IMarkerSet set1, IMarkerSet set2){
-		set1 = MarkerFactory.newBitMarkerSet();
-		set2 = MarkerFactory.newBitMarkerSet();
-	}
 	
-	public static void resetMarkerSet(IMarkerSet set1, IMarkerSet set2){
-		set1 = MarkerFactory.newMarkerSet();
-		set2 = MarkerFactory.newMarkerSet();
+	public static void  CloneTest() throws Exception{
+		CloneTestSets(10, 200);
+		CloneTestSets(100, 200);
+		CloneTestSets(1000, 200);
 	}
-	
-	public static void  DiffTest(IMarkerSet set1, IMarkerSet set2) throws Exception{
-		Random number = new Random();
-		int maxRel = 3, maxAttr = 3, maxTid = 99999;
-		
-		setToElement(set1, 10, maxRel, maxTid, maxAttr, number);
-		setToElement(set2, 10, maxRel, maxTid, maxAttr, number);
-		
-		long test1start = System.currentTimeMillis();
-		set1.diff(set2);
-		long test1end = System.currentTimeMillis();
-		
-		setToElement(set1, 100, maxRel, maxTid, maxAttr, number);
-		setToElement(set2, 100, maxRel, maxTid, maxAttr, number);
-		
-		long test2start = System.currentTimeMillis();
-		set1.diff(set2);
-		long test2end = System.currentTimeMillis();
-		
-		
-		setToElement(set1, 1000, maxRel, maxTid, maxAttr, number);
-		setToElement(set2, 1000, maxRel, maxTid, maxAttr, number);
-		
-		long test3start = System.currentTimeMillis();
-		set1.diff(set2);
-		long test3end = System.currentTimeMillis();
-		
-
-		log.debug("Union Between 2 Sets of 10 elements time: " + (test1end - test1start));
-		log.debug("Union Between 2 Sets of 100 elements time: " + (test2end - test2start));
-		log.debug("Union Between 2 Sets of 1000 elements time: " + (test3end - test3start));
-		
-	}
-	
-	public static void  IntersectTest(IMarkerSet set1, IMarkerSet set2) throws Exception{
-		Random number = new Random();
-		int maxRel = 3, maxAttr = 3, maxTid = 99999;
-		
-		setToElement(set1, 10, maxRel, maxTid, maxAttr, number);
-		setToElement(set2, 10, maxRel, maxTid, maxAttr, number);
-		
-		long test1start = System.currentTimeMillis();
-		set1.intersect(set2);
-		long test1end = System.currentTimeMillis();
-		
-		setToElement(set1, 100, maxRel, maxTid, maxAttr, number);
-		setToElement(set2, 100, maxRel, maxTid, maxAttr, number);
-		
-		long test2start = System.currentTimeMillis();
-		set1.intersect(set2);
-		long test2end = System.currentTimeMillis();
-		
-		
-		setToElement(set1, 1000, maxRel, maxTid, maxAttr, number);
-		setToElement(set2, 1000, maxRel, maxTid, maxAttr, number);
-		
-		long test3start = System.currentTimeMillis();
-		set1.intersect(set2);
-		long test3end = System.currentTimeMillis();
-		
-
-		log.debug("Union Between 2 Sets of 10 elements time: " + (test1end - test1start));
-		log.debug("Union Between 2 Sets of 100 elements time: " + (test2end - test2start));
-		log.debug("Union Between 2 Sets of 1000 elements time: " + (test3end - test3start));
-		
-	}
-	
 	
 	
 	public static void  UnionTest() throws Exception{
@@ -193,6 +67,93 @@ public class TestBitMarkerPerformance {
 		unionSets(100, 200);
 		unionSets(1000, 200);
 	}
+	
+	public static void  IntersectTest() throws Exception{
+		IntersectSets(10, 200);
+		IntersectSets(100, 200);
+		IntersectSets(1000, 200);
+	}
+	
+	public static void  DiffTest() throws Exception{
+		DiffSets(10, 200);
+		DiffSets(100, 200);
+		DiffSets(1000, 200);
+	}
+	
+	
+	public static void CloneTestSets (int size, int numSets) throws Exception {
+		Random number = new Random();
+		int maxRel = 3, maxAttr = 3, maxTid = 99999;
+		IMarkerSet[] sets;
+		IMarkerSet[] bitSets;
+		
+		sets = genSets(false, numSets, size, maxRel, maxTid, maxAttr, number);
+		bitSets = genSets(true, numSets, size, maxRel, maxTid, maxAttr, number);
+		
+		long test1start = System.currentTimeMillis();
+		for(int i = 0; i < sets.length; i+=2)
+			sets[i] = sets[i+1].cloneSet();
+		long test1end = System.currentTimeMillis();
+		
+		long test2start = System.currentTimeMillis();
+		for(int i = 0; i < bitSets.length; i+=2)
+			bitSets[i] = bitSets[i+1].cloneSet();
+		long test2end = System.currentTimeMillis();
+		
+		log.debug("------ SIZE " + size + " ------");
+		log.debug("MARKER: Cloning Between " + numSets + " Sets of " + size + " elements each time: " + (test1end - test1start));
+		log.debug("BITMARKER: Cloning Between " + numSets + " Sets of " + size + " elements each time: " + (test2end - test2start));
+	}
+	
+	public static void DiffSets (int size, int numSets) throws Exception {
+		Random number = new Random();
+		int maxRel = 3, maxAttr = 3, maxTid = 99999;
+		IMarkerSet[] sets;
+		IMarkerSet[] bitSets;
+		
+		sets = genSets(false, numSets, size, maxRel, maxTid, maxAttr, number);
+		bitSets = genSets(true, numSets, size, maxRel, maxTid, maxAttr, number);
+		
+		long test1start = System.currentTimeMillis();
+		for(int i = 0; i < sets.length; i+=2)
+			sets[i].diff(sets[i+1]);
+		long test1end = System.currentTimeMillis();
+		
+		long test2start = System.currentTimeMillis();
+		for(int i = 0; i < bitSets.length; i+=2)
+			bitSets[i].diff(bitSets[i+1]);
+		long test2end = System.currentTimeMillis();
+		
+		log.debug("------ SIZE " + size + " ------");
+		log.debug("MARKER: Difference Between " + numSets + " Sets of " + size + " elements each time: " + (test1end - test1start));
+		log.debug("BITMARKER: Difference Between " + numSets + " Sets of " + size + " elements each time: " + (test2end - test2start));
+	}
+	
+	public static void IntersectSets (int size, int numSets) throws Exception {
+		Random number = new Random();
+		int maxRel = 3, maxAttr = 3, maxTid = 99999;
+		IMarkerSet[] sets;
+		IMarkerSet[] bitSets;
+		
+		sets = genSets(false, numSets, size, maxRel, maxTid, maxAttr, number);
+		bitSets = genSets(true, numSets, size, maxRel, maxTid, maxAttr, number);
+		
+		long test1start = System.currentTimeMillis();
+		for(int i = 0; i < sets.length; i+=2)
+			sets[i].intersect(sets[i+1]);
+		long test1end = System.currentTimeMillis();
+		
+		long test2start = System.currentTimeMillis();
+		for(int i = 0; i < bitSets.length; i+=2)
+			bitSets[i].intersect(bitSets[i+1]);
+		long test2end = System.currentTimeMillis();
+		
+		log.debug("------ SIZE " + size + " ------");
+		log.debug("MARKER: Intersect Between " + numSets + " Sets of " + size + " elements each time: " + (test1end - test1start));
+		log.debug("BITMARKER: Intersect Between " + numSets + " Sets of " + size + " elements each time: " + (test2end - test2start));
+	}
+	
+	
 	
 	public static void unionSets (int size, int numSets) throws Exception {
 		Random number = new Random();
