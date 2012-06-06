d61c96a478175bf5988349098a09b39bf3a1cf9a
glavic <glavic@de4125fd-08af-4ef3-add0-b7c1c68fae66>
2012-06-06 17:15:20 +0000

    git-svn-id: https://dblab.cs.toronto.edu/svn/boriscode/TrampExGen/trunk@264 de4125fd-08af-4ef3-add0-b7c1c68fae66

---
+++ test/org/vagabond/test/xmlbeans/TestLoadExplanations.java
@@ -0,0 +1,19 @@
+package org.vagabond.test.xmlbeans;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.vagabond.test.AbstractVagabondTest;
+
+public class TestLoadExplanations extends AbstractVagabondTest {
+	
+	@Before
+	public void setUp () throws Exception {
+		loadToDB("resource/test/simpleTest.xml");
+	}
+
+	@Test
+	public void testSingleExpl () {
+		
+	}
+	
+}
