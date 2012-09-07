34aaf8952084e9744b7cfb7eb7d254c91ddc8de8
glavic <glavic@de4125fd-08af-4ef3-add0-b7c1c68fae66>
2012-09-07 15:59:59 +0000

    git-svn-id: https://dblab.cs.toronto.edu/svn/boriscode/TrampExGen/trunk@449 de4125fd-08af-4ef3-add0-b7c1c68fae66

---
+++ test/org/vagabond/test/xmlbeans/TestCreateXML.java
@@ -0,0 +1,31 @@
+package org.vagabond.test.xmlbeans;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.apache.xmlbeans.XmlCursor;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.vagabond.xmlmodel.RelAtomType;
+
+public class TestCreateXML {
+
+	static Logger log = Logger.getLogger(TestCreateXML.class);
+	
+	@BeforeClass
+	public static void setUp () {
+		PropertyConfigurator.configure("resource/test/testLog4jproperties.txt");
+	}
+	
+	@Test
+	public void testAddChoiceElementsToSeq () {
+		RelAtomType a = RelAtomType.Factory.newInstance();
+		a.addVar("a");
+		a.addNewSKFunction().setSkname("SK1");
+		a.addNewSKFunction().setSkname("SK2");
+		a.addVar("c");
+		XmlCursor x = a.newCursor();
+		x.toFirstChild();
+		log.debug(x.getObject());
+		log.debug(a);
+	}
+}
