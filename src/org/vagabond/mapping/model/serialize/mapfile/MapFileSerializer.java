81e4487fd312a1632c5de94a3b484c98f3f1a80b
glavic <glavic@de4125fd-08af-4ef3-add0-b7c1c68fae66>
2013-02-20 02:19:00 +0000

    git-svn-id: https://dblab.cs.toronto.edu/svn/boriscode/TrampExGen/trunk@554 de4125fd-08af-4ef3-add0-b7c1c68fae66

--- src/org/vagabond/mapping/model/serialize/mapfile/MapFileSerializer.java
+++ src/org/vagabond/mapping/model/serialize/mapfile/MapFileSerializer.java
@@ -8,6 +8,9 @@ import org.apache.xmlbeans.XmlObject;
 import org.apache.xmlbeans.XmlString;
 import org.vagabond.mapping.model.MapScenarioHolder;
 import org.vagabond.xmlmodel.AttrDefType;
+import org.vagabond.xmlmodel.AttrListType;
+import org.vagabond.xmlmodel.AttrRefType;
+import org.vagabond.xmlmodel.ForeignKeyType;
 import org.vagabond.xmlmodel.FunctionType;
 import org.vagabond.xmlmodel.MappingScenarioDocument.MappingScenario;
 import org.vagabond.xmlmodel.MappingType;
@@ -34,9 +37,9 @@ public class MapFileSerializer {
 	public String transformToMap (MappingScenario doc) throws Exception {
 		StringBuilder result = new StringBuilder();
 		
-		result.append("# name of mapping\n");
+		result.append("################# NAME OF MAPPING ##########################\n");
 		result.append("Name: \n");
-		result.append("# source and target schema\n");
+		result.append("################# SOURCE AND TARGET SCHEMA #################\n");
 		
 		// output schemas
 		result.append("Source:\n");
@@ -45,10 +48,65 @@ public class MapFileSerializer {
 		outputSchema(doc.getSchemas().getTargetSchema(), result);
 		
 		// output mappings
-		result.append("\n# constraints\n");
+		result.append("\n############### MAPPINGS ################################\n");
 		outputMappings(doc.getMappings(), result);
 		
+		// output PK and foreign key constraints
+		result.append("\n############### PK CONSTRAINTS ##########################\n");
+		outputPKConstraints(doc, result);
+		result.append("\n############### FK CONSTRAINTS ##########################\n");
+		outputFKConstraints(doc, result);
+		
 		return result.toString();
+	}
+
+	private void outputFKConstraints(MappingScenario doc, StringBuilder result) {
+		result.append("\n## SOURCE SCHEMA\n");
+		outputFKConstraints(doc.getSchemas().getSourceSchema(), result);
+		result.append("\n## TARGET SCHEMA\n");
+		outputFKConstraints(doc.getSchemas().getTargetSchema(), result);
+	}
+
+	private void outputFKConstraints(SchemaType schema,StringBuilder result) {
+		for(ForeignKeyType fk: schema.getForeignKeyArray()) {
+			result.append("# ");
+			outputFKPart(fk.getFrom(),result);
+			result.append(" -> ");
+			outputFKPart(fk.getTo(),result);
+			result.append("\n");
+		}
+		
+	}
+
+	private void outputFKPart(AttrRefType a, StringBuilder result) {
+		result.append(a.getTableref() + "(");
+		for(String at: a.getAttrArray()) {
+			result.append(at);
+			result.append(", ");
+		}
+		result.delete(result.length() - 2, result.length());
+		result.append(")");
+	}
+
+	private void outputPKConstraints(MappingScenario doc, StringBuilder result) {
+		result.append("\n## SOURCE SCHEMA\n");
+		outputPKConstraints(doc.getSchemas().getSourceSchema(), result);
+		result.append("\n## TARGET SCHEMA\n");
+		outputPKConstraints(doc.getSchemas().getTargetSchema(), result);	
+	}
+
+	private void outputPKConstraints(SchemaType schema, StringBuilder result) {
+		for(RelationType r: schema.getRelationArray()) {
+			result.append("# " + r.getName() + "(");
+			
+			for(String a: r.getPrimaryKey().getAttrArray()) {
+				result.append(a);
+				result.append(", ");
+			}
+			
+			result.delete(result.length() - 2, result.length());
+			result.append(")\n");
+		}		
 	}
 
 	private void outputMappings(MappingsType mappings, StringBuilder buf) throws Exception {
