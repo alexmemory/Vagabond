78867539b82b61b2b3feb883c703ec2ef46c5b8c
iypearun001 <amathewi@hawk.iit.edu>
2013-10-22 16:21:59 -0500

    implemented DBMarkerSet Set output type methods

---
+++ src/org/vagabond/explanation/marker/IDBMarkerStrategy.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package org.vagabond.explanation.marker;
+
+import java.util.Set;
+
+import org.vagabond.util.Enums.Marker_Type;
+
+/**
+ * @author lord_pretzel
+ *
+ */
+public interface IDBMarkerStrategy {
+
+	public enum CreateStrategy {
+		CreateOnSource,
+		CreateOnTarget
+	}
+	
+	public Set<Marker_Type> getBinaryOperationOutput (Set<Marker_Type> leftTypes, Set<Marker_Type> rightTypes);
+	public void setBinaryOperationOutput(Set<Marker_Type> leftTypes, Set<Marker_Type> rightTypes, Set<Marker_Type> outTypes);
+	public CreateStrategy getCreateStrategy ();
+	public CreateStrategy setCreateStrategy (CreateStrategy strat);
+	
+}
