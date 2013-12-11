1ccbaeba9a5a51c5edd6cb2883118f8671bf0314
iypearun001 <amathewi@hawk.iit.edu>
2013-12-11 13:53:34 -0600

    Performance Testing : Convert, Add Single Element, Union

--- performanceTest/TestDBMarkerSetPerformance.java
+++ performanceTest/TestDBMarkerSetPerformance.java
@@ -33,7 +33,7 @@ public class TestDBMarkerSetPerformance {
 		Clone
 	}
 
-	private static int[] sizesForSetTests = new int[] {100,200};//, 2000, 4000, 5000, 10000};
+	private static int[] sizesForSetTests = new int[] {100,1000, 2000, 4000, 5000, 10000};
 	private static int repeatCount = 2;
 
 	public static void main (String[] args) throws Exception {
@@ -44,11 +44,75 @@ public class TestDBMarkerSetPerformance {
 		loadToDB("resource/exampleScenarios/homeless3.xml");
 		
 		//Prints out the avg taken taken for converting one type to another
-		generationTest();
+		//generationTest();
 				
+		//SingleAdditionTest();
+		
+		unionTest();
 		if (log.isDebugEnabled()) {log.debug("\t-- Generated --");};
 	}
 
+	private static void unionTest() throws Exception {
+		int maxAttr = 15;
+		int maxTid = 10000;
+
+		//TABLE to JAVA REP
+		System.out.print("\n\t--Union TABLE --");
+		int loop;
+		unionDBMarkerSet(maxAttr, maxTid, Marker_Type.TABLE_REP , Marker_Type.JAVA_REP,"");
+		
+		//JAVA to TABLE REP
+		System.out.print("\n\t--Union JAVA to TABLE REP--");
+		unionDBMarkerSet(maxAttr, maxTid, Marker_Type.JAVA_REP , Marker_Type.TABLE_REP,"");
+		
+		//INDEXED QUERY to JAVA REP
+		System.out.print("\n\t--Union INDEXED QUERY --");
+		unionDBMarkerSet(maxAttr, maxTid, Marker_Type.QUERY_REP , Marker_Type.JAVA_REP,"select 'table10000_15' :: text,  tid, '0100000000000000' ::bit varying  from source.table10000_15 where col1<");
+		
+		
+		//NON INDEXED QUERY to JAVA REP
+		System.out.print("\n\t--Union NON INDEXED  QUERY--");
+		unionDBMarkerSet(maxAttr, maxTid, Marker_Type.QUERY_REP , Marker_Type.TABLE_REP,"select 'table10000_15' :: text,  tid, '0100000000000000' ::bit varying  from source.table10000_15 where tid<");
+	}
+
+
+
+	private static long[] unionDBMarkerSet(int maxAttr, int maxTid,Marker_Type source, Marker_Type dest, String query)
+			throws Exception {
+		int loop=0;
+		long[] times = new long[sizesForSetTests.length];
+		
+		for(int i=0;i<sizesForSetTests.length;i++)
+		{
+		  times[i]=0;
+		}
+		
+		//Repeat the following tests k times to get a good average time estimate. k =  repeatCount
+		while(loop<repeatCount)
+		{
+			DBMarkerSet[] testset = genSets(source, "table10000_15",maxAttr, maxTid, query);
+			DBMarkerSet[] testset2 = genSets(source, "table10000_15",maxAttr, maxTid, query);
+			for(int i=0;i<sizesForSetTests.length;i++)
+			{
+				DBMarkerSet temp =testset[i];
+				DBMarkerSet temp1 =testset[i];
+				long before = System.currentTimeMillis();
+				temp.union(temp1);
+				long after = System.currentTimeMillis();
+				times[i] += after - before; 
+				//if (log.isDebugEnabled()) {log.debug("\t-- Size :" + sizesForSetTests[i] + " Time : " + times[i]);};
+			}
+			loop++;
+		}
+		
+		
+		for(int i=0;i<sizesForSetTests.length;i++)
+		{
+		  times[i]/=repeatCount;
+		  System.out.print("\n\t-- Size :" + sizesForSetTests[i] + " Avg Time : " + times[i]);
+		}
+		return times;
+	}
 
 
 	private static void generationTest() throws Exception {
@@ -120,7 +184,68 @@ public class TestDBMarkerSetPerformance {
 	}
 
 
+	private static void SingleAdditionTest() throws Exception {
+		int maxAttr = 15;
+		int maxTid = 10000;
 
+		//TABLE
+		System.out.print("\n\t--Addition to TABLE DBMarkerSet--");
+		int loop;
+		addToDBMarkerSet(maxAttr, maxTid, Marker_Type.TABLE_REP , Marker_Type.JAVA_REP,"");
+		
+		//JAVA 
+		System.out.print("\n\t--Addition to JAVA  DBMarkerSet--");
+		addToDBMarkerSet(maxAttr, maxTid, Marker_Type.JAVA_REP , Marker_Type.TABLE_REP,"");
+		
+		// INDEXED QUERY
+		System.out.print("\n\t--Addition to  INDEXED QUERY  DBMarkerSet--");
+		addToDBMarkerSet(maxAttr, maxTid, Marker_Type.QUERY_REP , Marker_Type.JAVA_REP,"select 'table10000_15' :: text,  tid, '0100000000000000' ::bit varying  from source.table10000_15 where col1<");
+		
+		//NON INDEXED QUERY to JAVA REP
+		System.out.print("\n\t--Addition to NON INDEXED QUERY  DBMarkerSet--");
+		addToDBMarkerSet(maxAttr, maxTid, Marker_Type.QUERY_REP , Marker_Type.JAVA_REP,"select 'table10000_15' :: text,  tid, '0100000000000000' ::bit varying  from source.table10000_15 where tid<");
+		
+		
+	}
+	
+	private static long[] addToDBMarkerSet(int maxAttr, int maxTid,Marker_Type source, Marker_Type dest, String query)
+			throws Exception {
+		int loop=0;
+		long[] times = new long[sizesForSetTests.length];
+		
+		for(int i=0;i<sizesForSetTests.length;i++)
+		{
+		  times[i]=0;
+		}
+		
+		//Repeat the following tests k times to get a good average time estimate. k =  repeatCount
+		while(loop<repeatCount)
+		{
+			DBMarkerSet[] testset = genSets(source, "table10000_15",maxAttr, maxTid, query);
+			for(int i=0;i<sizesForSetTests.length;i++)
+			{
+				DBMarkerSet temp =testset[i];
+				IAttributeValueMarker marker = randMarker("table10000_15", maxTid, maxAttr, new Random(0));
+				long before = System.currentTimeMillis();
+				
+				temp.add(marker);
+				long after = System.currentTimeMillis();
+				times[i] += after - before; 
+				//if (log.isDebugEnabled()) {log.debug("\t-- Size :" + sizesForSetTests[i] + " Time : " + times[i]);};
+			}
+			loop++;
+		}
+		
+		
+		for(int i=0;i<sizesForSetTests.length;i++)
+		{
+		  times[i]/=repeatCount;
+		  System.out.print("\n\t-- Size :" + sizesForSetTests[i] + " Avg Time : " + times[i]);
+		}
+		return times;
+	}
+	
+	
 	private static DBMarkerSet[] genSets (Marker_Type type, String relName, int maxAttr, int maxTid, String query) throws Exception 
 	{
 		DBMarkerSet[] result = new DBMarkerSet[sizesForSetTests.length];
